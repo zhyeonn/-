4797e0b95a62f7126b2044eaec983b111cadc45c
SangIn Jeong <sanginjeong07@gmail.com>
2024-01-08 20:34:28 +0900

    셀 병합 수정 전 카피

--- timetable.js
+++ timetable.js
@@ -10,7 +10,7 @@ const getDay = (index) => {
 
 const makeTimetable = (grade) => {
   const timetable = document.querySelector(`#timetable-${grade}`)
-  for(let i=9; i<=18; i++){
+  for(let i=9; i<18; i++){
     const hour = `${i}:00 ~ ${i+1}:00`
     const row = document.createElement('tr');
     const timecell = document.createElement('th');
@@ -31,7 +31,7 @@ makeTimetable(2);
 makeTimetable(3);
 makeTimetable(4);
 
-// 클릭한 버튼의 index+1을 해서 timetable-(index+1) 의 요소 수정
+
 let selectGrade = '';
 $addTime.forEach((addbtn,index)=>{
   addbtn.addEventListener('click',()=>{
@@ -43,14 +43,25 @@ $addTime.forEach((addbtn,index)=>{
 })
 
 $completeTime.addEventListener('click',()=>{
-  const lectureName = document.querySelector('#lecture_name').value;  
+  let lectureName = document.querySelector('#lecture_name').value;  
+  if(lectureName === ''){
+    alert('강의명을 입력하세요');
+    return
+  }
+  
   let day = '';
 
+  let isChecked = false;
   document.getElementsByName('radio').forEach((radio)=>{
     if(radio.checked === true){
       day = radio.value;
+      isChecked = true;
     }
   })
+  if(!isChecked){
+    alert('요일을 선택하세요');
+    return
+  }
 
   let startTime = +getstartTime();
   let endTime = +getEndTime();
@@ -58,14 +69,12 @@ $completeTime.addEventListener('click',()=>{
     alert('종료시간이 시작시간보다 앞섭니다.');
     return
   }
-  
-  const selectedColor = document.querySelector('#select_color').value;
+  let selectedColor = document.querySelector('#select_color').value;
 
   // 시간표 나타내기
   renderTimeTable(lectureName,day,startTime,endTime,selectedColor);
   // render 후 초기화
-  $inputTime.style.display = 'none';
-  $overlay.classList.remove('active');
+  clearinputTimeModal();
 }) 
 
 const getstartTime = () => {
@@ -79,16 +88,52 @@ const getEndTime = () => {
   return endTime
 }
 
-//  colspan은 가로 방향으로의 병합을, rowspan은 세로 방향
-const renderTimeTable = (lectureName,day,startTime,endTime,selectedColor) => {
-  for(let i=startTime; i<endTime; i++){
-    const $timetable = document.querySelector(`#${day}-${selectGrade}-${i}`);
-    $timetable.textContent = lectureName;
-    $timetable.style.backgroundColor = selectedColor;
+const renderTimeTable = (lectureName,day,startTime,endTime,selectedColor) => {  
+  const $timetable= document.querySelector(`#timetable-${selectGrade}`);
+  const cellToMerge = $timetable.rows[startTime-9+1].cells[days.indexOf(day)+1]
+  console.log(cellToMerge);
+  cellToMerge.setAttribute('rowspan',`${endTime-startTime}`)
+
+  // display : none 하니까 edit할때 안될 수도
+  for(let i=startTime+1; i<endTime; i++){
+    $timetable.rows[i-9+1].cells[days.indexOf(day)+1].style.display ='none';
   }
-  
-  // const $timetable= document.querySelector(`#timetable-${selectGrade}`);
-  // const cellToMerge = $timetable.rows[startTime-9+1].cells[days.indexOf(day)+1]
-  // cellToMerge.setAttribute('rowspan',`${endTime-startTime}`)
-  // day를 index로 불러와서 딱 위치 지정해서 end-start만큼 병합
+
+  cellToMerge.innerHTML = `
+  <div class = "btns">
+    <button onclick="editSchedule()"><i class="fa-solid fa-pen"></i></button>
+    <button onclick="deleteSchedule()"><i class="fa-solid fa-trash"></i></button>
+  </div>
+  <div>${lectureName}</div>
+  `
+  cellToMerge.style.backgroundColor = selectedColor;
+}
+
+// 현재 여러번 클릭하면 overlay active가 활성화된다.
+const clearinputTimeModal = () => {
+  $inputTime.style.display = 'none';
+  $overlay.classList.remove('active');
+  document.querySelector('#lecture_name').value ='';
+  document.getElementsByName('radio').forEach((radio)=>{
+    if(radio.checked === true){
+      radio.checked = false;
+    }
+  })
+  document.querySelector('#start_time').selectedIndex = 0;
+  document.querySelector('#end_time').selectedIndex = 0;
+  document.querySelector('#select_color').value = 0;
+}
+
+const editSchedule = () => {
+  // 모달과 오버레이를 다시 연다. 입력한 정보와 시간을 채워서 연다.
+
+  // 문제는 겹치는 시간이 있을 때 등록안되게 해놓을텐데 여기도 적용됨
+}
+
+const deleteSchedule = () => {
+
+}
+
+const clearTimetable = () => {
+
 }
